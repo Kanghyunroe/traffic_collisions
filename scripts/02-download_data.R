61a9bf2898256e5390fb78d269893775cfd29eed
Kevin Roe <kevinroe0124@gmail.com>
2024-11-25 23:49:03 -0500

    updated dataset download script

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -27,8 +27,8 @@ library(arrow)
 # datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
 
 # Load the first datastore resource as a sample
-data <- read_csv("https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/ec53f7b2-769b-4914-91fe-a37ee27a90b3/resource/3fed26a8-62fa-4c7c-8e76-9bacb6401d16/download/Traffic%20Collisions%20-%202952.csv")
-
+data <- 
+  read_csv("https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/ec53f7b2-769b-4914-91fe-a37ee27a90b3/resource/cb890861-ed20-4862-bb75-b1f9ec1e58dd/download/Traffic%20Collisions%20-%204326.csv")
 #### Save data ####
 write_parquet(data, "data/01-raw_data/raw_data.parquet") 
 
